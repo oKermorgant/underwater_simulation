7b54dc4d70bbb59a369de79cd1477b01be9d863d
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2020-06-15 14:35:53 +0200

    without build directories...

--- 3rd_parties/uwsim_osgworks-release/build/lib/osgWorksBuildTreeSettings.cmake
+++
@@ -1,3 +0,0 @@
-set(OSGWORKS_INCLUDE_DIRS
-  "/home/olivier/code/libs/ros/src/underwater_simulation/3rd_parties/uwsim_osgworks-release/include"
-  "/home/olivier/code/libs/ros/src/underwater_simulation/3rd_parties/uwsim_osgworks-release/build")
--- 3rd_parties/uwsim_osgworks-release/build/qtcsettings.cmake
+++
@@ -1,6 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/usr" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/usr/lib/qt5/bin/qmake" CACHE "STRING" "" FORCE)
